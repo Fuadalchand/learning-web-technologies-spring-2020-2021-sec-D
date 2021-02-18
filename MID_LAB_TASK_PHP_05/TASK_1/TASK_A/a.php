725a2dee136357f598c23b675c06668184cef415
Fuadalchand <Fachand26@gmail.com>
2021-02-18 23:18:04 +0600

    task_1_A

---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_A/a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>My Name</title>
+</head>
+<body>
+
+<form action="Next.php" method="post">
+	<fieldset>
+		<legend>NAME</legend>
+Name: <input type="text" name="myname"><br>
+<input type="submit">
+	</fieldset>
+</form>
+
+</body>
+</html>
