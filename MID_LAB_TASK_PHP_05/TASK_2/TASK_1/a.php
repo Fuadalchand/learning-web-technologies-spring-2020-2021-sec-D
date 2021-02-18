49f0356f478605bb75b29fe5ed4ff7ffb89fb267
Fuadalchand <Fachand26@gmail.com>
2021-02-19 01:17:12 +0600

    TASK_2_A

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_1/a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>My E-mail</title>
+</head>
+<body>
+
+<form action="mail.php" method="post">
+	<fieldset>
+		<legend>EMAIL</legend>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</fieldset>
+</form>
+
+</body>
+</html>
