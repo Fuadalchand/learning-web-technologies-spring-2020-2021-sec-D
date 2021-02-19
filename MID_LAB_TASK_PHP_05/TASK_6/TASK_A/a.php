c359188c9d1048a3d6dcca7edd7333b6873ff545
Fuadalchand <Fachand26@gmail.com>
2021-02-19 13:27:36 +0600

    TASK_6

---
+++ MID_LAB_TASK_PHP_05/TASK_6/TASK_A/a.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Blood Group</title>
+</head>
+<body>
+
+<form action="blood.php" method="post">
+	<fieldset>
+		<legend>BLOOD GROUP</legend>
+Blood Group: 
+<select name="blood">
+<option value=""> --Option--</option>
+<option value="A+"> A+ </option>
+<option value="A-"> A- </option>
+<option value="B+"> B+ </option>
+<option value="B-"> B- </option>
+<option value="O+"> O+ </option>
+<option value="O-"> O- </option>
+<option value="AB+"> AB+ </option>
+<option value="AB-"> AB- </option>
+
+
+</select>
+<br>
+<input type="submit">
+</fieldset>
+</form>
+
+</body>
+</html>
