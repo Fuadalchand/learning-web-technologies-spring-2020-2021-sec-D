0c4e476728ada31b843d8bf69d83f60ea97076be
Fuadalchand <Fachand26@gmail.com>
2021-02-19 13:14:49 +0600

    labtask_5_A

---
+++ MID_LAB_TASK_PHP_05/TASK_5/TASK_A/a.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Degree</title>
+</head>
+<body>
+
+<form action="degree.php" method="post">
+	<fieldset>
+		<legend>DEGREE</legend>
+Degree: 
+SSC<input type="checkbox" value="SSC" name="degree">
+HSC<input type="checkbox" value="HSC" name="degree">
+BSc<input type="checkbox" value="BSc" name="degree">
+MSc<input type="checkbox" value="MSc" name="degree">
+<br>
+<input type="submit">
+</fieldset>
+</form>
+
+</body>
+</html>
